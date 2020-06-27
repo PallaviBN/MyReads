70342969b16fa4bb9aece7aa71870ee40b54b7ad
Pallavi Nagarkar <66208146+PallaviBN@users.noreply.github.com>
2020-06-27 12:17:45 +0530

    Second commit

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -20,7 +20,7 @@ export const get = (bookId) =>
 export const getAll = () =>
   fetch(`${api}/books`, { headers })
     .then(res => res.json())
-    .then(data => data.books)
+    .then(data => data.books);
 
 export const update = (book, shelf) =>
   fetch(`${api}/books/${book.id}`, {
@@ -32,13 +32,13 @@ export const update = (book, shelf) =>
     body: JSON.stringify({ shelf })
   }).then(res => res.json())
 
-export const search = (query) =>
+export const search = (query, maxResults) =>
   fetch(`${api}/search`, {
     method: 'POST',
     headers: {
       ...headers,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ query })
+    body: JSON.stringify({ query , maxResults})
   }).then(res => res.json())
     .then(data => data.books)
